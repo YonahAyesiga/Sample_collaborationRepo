a10236989f7bbb55d3052a4a0508ec098ca03355
YonahAyesiga <yonahayesiga241@gmail.com>
2025-07-27 17:45:32 +0300

    my initial commit

---
+++ db_connection.php
@@ -0,0 +1,13 @@
+<?php
+// connect to the database
+$connection = mysqli_connect("localhost", "root", "", "db_name");
+
+// display an error message if the connection fails
+if(mysqli_connect_errno()){
+    echo "Connection to the database failed " . mysqli_connect_errno();
+
+}
+
+?>
+
+
---
+++ helo.php
@@ -0,0 +1,4 @@
+<?php
+$name = "Yonah";
+echo "Hello" . name;
+?>
